be6c37927b981169ecfd3929c3c273c6020cfddd
escowin <edwin@escowinart.com>
2021-11-28 12:02:54 -0600

    js password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,23 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("this works")
 
-  //pw criteria prompt: LENGTH 8 < 128 CHARACTER LIMIT
-
+  //pw criteria prompt: LENGTH 8 < 128 CHARACTER LIMIT. window prompt
+  var passwordLength = prompt("Enter password length within the range of 8 - 128 characters.");
+  if (passwordLength < 8) {
+    alert('Password must be at least 8 characters.');
+    return;
+  }
+  if (passwordLength > 128) {
+    alert('Password must be less than 128 characters.');
+    return;
+  } 
+  }
   //pw criteria prompt: include LOWER, UPPER, NUM, SPECIAL CHARACTER options
-
   //validate input
 
-  //display generated password using above selected criteria
+  //generate password using above selected criteria
 
   //display password
-  return "test test"
-}
 
 // Write password to the #password input
 function writePassword() {
