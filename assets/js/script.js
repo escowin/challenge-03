babbcaea7d86e539b2a5ae8c618bef548534c2e6
escowin <edwin@escowinart.com>
2021-11-28 13:21:31 -0600

    js randomize and pw generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var numeric = ['0','1','2','3','4','5','6','7','8','9']
-var specialCharacters = ['!','@','#','$','%','^','&','*',"(",")",'_','+']
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var numericArray = ['0','1','2','3','4','5','6','7','8','9']
+var specialCharactersArray = ['!','@','#','$','%','^','&','*',"(",")",'_','+']
 
 function generatePassword() {
   console.log("this works")
@@ -44,9 +44,53 @@ function generatePassword() {
   console.log(passwordOptions)
   return passwordOptions;
   }
-  //generate password using above selected criteria
 
+  //randomize characters
+  function random(arr) {
+    var index = Math.floor(Math.random()* arr.length)
+    var element = arr[index]
+    return element
+  }
+  console.log(random(specialCharactersArray))
+
+//generate password using above selected criteria
+//function generatePassword() {
+  var choices = generatePassword()
+  var finalResult = []
+  var possibleCharacters = []
+  var guaranteeCharacters = []
+  if(choices.lowerCase) {
+    possibleCharacters= possibleCharacters.concat(lowerCaseArray);
+    guaranteeCharacters.push(random(lowerCaseArray));
+  }
+  if(choices.upperCase) {
+    possibleCharacters= possibleCharacters.concat(upperCaseArray)
+    guaranteeCharacters.push(random(upperCaseArray));
+  }
+  if(choices.numeric) {
+    possibleCharacters= possibleCharacters.concat(numericArray)
+    guaranteeCharacters.push(random(numericArray));
+  }
+  if(choices.specialCharacters) {
+    possibleCharacters= possibleCharacters.concat(specialCharactersArray)
+    guaranteeCharacters.push(random(specialCharactersArray))
+  }
+
+  //all arrays randomized in possibleCharacters
+  for(var i = 0; i < choices.length; i++) {
+    var character = random(possibleCharacters)
+    finalResult[i] = guaranteeCharacters
+  }
+  //selected arrays in guaranteeCharacters
+  for(var i = 0; i < guaranteeCharacters.length; i++) {
+    finalResult[i] = guaranteeCharacters[i]
+  }
+  //combine guaranteeCharacters with random
+  return finalResult.join("")
+}
   //display password
+
+
 
 // Write password to the #password input
 function writePassword() {
