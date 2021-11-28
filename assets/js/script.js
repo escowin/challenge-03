8e98a52a87c1396e8901106c4b419d948efe66a4
escowin <edwin@escowinart.com>
2021-11-27 23:25:28 -0600

    js pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("this works")
+
+  //pw criteria prompt: LENGTH 8 < 128 CHARACTER LIMIT
+
+  //pw criteria prompt: include LOWER, UPPER, NUM, SPECIAL CHARACTER options
+
+  //validate input
+
+  //display generated password using above selected criteria
+
+  //display password
+  return "test test"
 }
 
 // Write password to the #password input
