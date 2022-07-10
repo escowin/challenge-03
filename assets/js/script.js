0197f8d1fe5350a5f92d2484844a24cb8aef23d9
escowin <edwin@escowinart.com>
2022-07-10 00:14:11 -0500

    variables & functions invoked successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
 // Assignment code here
 
+// logic.password generation
+// -  click <#generate>
+//    - prompts.select criteria:
+//      - length (8 - 128 char limit)
+//      - character types (lowercase, uppercase, numeric, and/or special char)
+//     - validate each selected criteria && 1 of each char type
+//     - generate password
+// logic.display password
+//  - window.alert || dom
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+
+var generatePassword = "this is the password";
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var writePassword = function() {
 
+  var password = generatePassword;
+  console.log(password)
   passwordText.value = password;
-
+  console.log(passwordText.value)
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", writePassword(), true);
