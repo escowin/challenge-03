0cef4c407283d362d836dc355a89389e9f627aa4
escowin <edwin@escowinart.com>
2022-07-16 10:09:11 -0500

    redux

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,14 @@
-// Assignment code here
-
-// logic.password generation
-// -  click <#generate>
-//    - prompts.select criteria:
-//      - length (8 - 128 char limit)
-//      - character types (lowercase, uppercase, numeric, and/or special char)
-//     - validate each selected criteria && 1 of each char type
-//     - generate password
-// logic.display password
-//  - window.alert || dom
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-
-var generatePassword = "this is the password";
 
 // Write password to the #password input
-var writePassword = function() {
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  var password = generatePassword;
-  console.log(password)
   passwordText.value = password;
-  console.log(passwordText.value)
+
 }
 
-
-
 // Add event listener to generate button
-
-generateBtn.addEventListener("click", writePassword(), true);
+generateBtn.addEventListener("click", writePassword);
