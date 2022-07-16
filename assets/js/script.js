e2cf20ad3c8edf1dce3e9dee12e43a309caaff46
escowin <edwin@escowinart.com>
2022-07-16 10:18:37 -0500

    js:11, 4-8 | ...eventlistener() > writePassword() > generatePassword()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generatePassword = function() {
+    console.log("generatePassword() runs");
+    return "this is the password.";
+}
+
 // Write password to the #password input
 function writePassword() {
+  console.log("writePassword() runs")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
