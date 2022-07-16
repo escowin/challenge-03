422faf504ad7e503325f4377ad72ec6c571a1d83
escowin <edwin@escowinart.com>
2022-07-16 13:03:57 -0500

    js:8-17 | moved prompts from writePassword() to generatePassword()

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Logic
 var generatePassword = function() {
-    return "this is the password.";
+    // password character prompts
+    var includeLowerCase = window.confirm("include lowercase?");
+    var includeUpperCase = window.confirm("include uppercase?");
+    var includeNumeric = window.confirm("include numeric?");
+    var includeSpecialCharcters = window.confirm("include special characters?");
+    if (includeLowerCase, includeUpperCase, includeNumeric, includeSpecialCharcters) {
+      return "this is the password.";
+    } else {
+      return "nothing";
+    }
 }
 
 // Write password to the #password input
 function writePassword() {
-  // password character prompts
-  var includeLowerCase = window.confirm("include lowercase?");
-  var includeUpperCase = window.confirm("include uppercase?");
-  var includeNumeric = window.confirm("include numeric?");
-  var includeSpecialCharcters = window.confirm("include special characters?");
-  console.log(
-    includeLowerCase, 
-    includeUpperCase, 
-    includeNumeric, 
-    includeSpecialCharcters
-  );
-
-  // password value
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
