9ed3a6b1670090cd527f15b936a358976df33510
escowin <edwin@escowinart.com>
2022-07-16 12:54:49 -0500

    js:14-17 | (+) password character prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character Arrays
+
+// Logic
 var generatePassword = function() {
-    console.log("generatePassword() runs");
     return "this is the password.";
 }
 
 // Write password to the #password input
 function writePassword() {
-  console.log("writePassword() runs")
+  // password character prompts
+  var includeLowerCase = window.confirm("include lowercase?");
+  var includeUpperCase = window.confirm("include uppercase?");
+  var includeNumeric = window.confirm("include numeric?");
+  var includeSpecialCharcters = window.confirm("include special characters?");
+  console.log(
+    includeLowerCase, 
+    includeUpperCase, 
+    includeNumeric, 
+    includeSpecialCharcters
+  );
+
+  // password value
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
