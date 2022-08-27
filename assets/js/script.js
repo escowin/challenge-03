2f93145f8e2942c234c1a49639da1a4c0333a9e1
escowin <edwin@escowinart.com>
2022-08-27 18:24:04 -0500

    js | restructured the logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // data.dom
 const generatorFormEl = document.getElementById("generator");
+const passwordDisplayEl = document.getElementById("password-display");
 const rangeSliderEl = document.getElementById("range-slider");
 const numericInputEl = document.getElementById("numeric-input");
 const lowercaseEl = document.getElementById("lowercase");
@@ -32,138 +33,34 @@ const syncedValues = function(e) {
 rangeSliderEl.addEventListener('input', syncedValues);
 numericInputEl.addEventListener('input', syncedValues);
 
-// logic.generating the password
-const generatePassword = function(passwordLength, includeLowercase, includeUppercase, includeNumerals, includeSymbols) {
-  var length = passwordLength;
-  console.log(length);
-
-  if (includeLowercase === true) {
-    console.log("lowercase selected")
-  }
-  if (includeUppercase === true) {
-    console.log("uppercase selected")
-  }
-  if (includeNumerals === true) {
-    console.log("numerals selected")
-  }
-  if (includeSymbols === true) {
-    console.log("symbols selected")
-  }
-
+// logic.generating password
+const generatePassword = function(length, selectedLowercase, selectedUppercase, selectedNumerals, selectedSymbols) {
+  selectedLowercase = lowercaseArr;
+  selectedUppercase = uppercaseEl;
+  selectedNumerals = numeralsArr;
+  selectedSymbols = symbolsArr;
+  
+  console.log (selectedLowercase, selectedLowercase, selectedNumerals, selectedSymbols);
+  return `generating password that is ${length} characters in length...`;
 };
 
 // event-listener.checks criteria, calls generatePassword()
 generatorFormEl.addEventListener('submit', e => {
   e.preventDefault();
 
-  const passwordLength = numericInputEl.value;
-  const includeLowercase = lowercaseEl.checked;
-  const includeUppercase = uppercaseEl.checked;
-  const includeNumerals = numeralsEl.checked;
-  const includeSymbols = symbolsEl.checked;
+  const length = numericInputEl.value;
+  const selectedLowercase = lowercaseEl.checked;
+  const selectedUppercase = uppercaseEl.checked;
+  const selectedNumerals = numeralsEl.checked;
+  const selectedSymbols = symbolsEl.checked;
 
-  const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumerals, includeSymbols);
+  if (!selectedLowercase && !selectedUppercase && !selectedNumerals && !selectedSymbols) {
+    passwordDisplayEl.textContent = "your password would be " + length + " characters if you select at least one of the boxes.";
+  } else {
+    const password = generatePassword(length, selectedLowercase, selectedUppercase, selectedNumerals, selectedSymbols);
+    passwordDisplayEl.textContent = password;
+  }
 });
-
-
-// logic.password-criteria
-// var passwordCriteria = function() {
-//   // password length prompt followed by if conditional statement to check if length falls outside acceptable range.
-//   var length = parseInt(window.prompt("set a password length between 8 - 128 characters long"));
-//   if (length < 8 || length > 128) {
-//     window.alert("password length outside acceptable range.")
-//     return null;
-//   }
-
-//   // password character prompts
-//   var includeLowerCase = window.confirm("include lowercase?");
-//   var includeUpperCase = window.confirm("include uppercase?");
-//   var includeNumeric = window.confirm("include numeric?");
-//   var includeSymbols = window.confirm("include symbols?");
-
-//   // conditional, value is null if no characters arrays are selected
-//   if (
-//     includeLowerCase === false && 
-//     includeUpperCase === false && 
-//     includeNumeric === false && 
-//     includeSymbols === false
-//   ) {
-//     return null;
-//   }
-
-//   // user input
-//   var selectedCriteria = {
-//     length: length,
-//     includeLowerCase: lowerCasedCharacters,
-//     includeUpperCase: includeUpperCase,
-//     includeNumeric: includeNumeric,
-//     includeSymbols: includeSymbols
-//   };
-
-//   return selectedCriteria;
-// }
-
-// logic | random
-// var getRandom = function(array) {
-//   var randomIndex = Math.floor(Math.random() * array.length);
-//   var randomElement = array[randomIndex];
-
-//   return randomElement;
-// }
-
-// Logic | password generated dependent on criteria selected.
-// var generatePassword = function() {
-//   var criteria = passwordCriteria();
-  
-//   var result = [];
-//   var possibleCharacters = [];
-//   var guaranteedCharacters = [];
-
-//   // conditional statement - checking if criteria 
-//   if (!criteria) return "new password could not be generated, try again";
-
-//   // -  lowercase
-//   if (criteria.includeLowerCase) {
-//     possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
-//     guaranteedCharacters.push(getRandom(lowerCasedCharacters));
-//   }
-
-//   // -  uppercase
-//   if (criteria.includeUpperCase) {
-//     possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
-//     guaranteedCharacters.push(getRandom(upperCasedCharacters));
-//   }
-//   // -  numeric
-//   if (criteria.includeNumeric) {
-//     possibleCharacters = possibleCharacters.concat(numericCharacters);
-//     guaranteedCharacters.push(getRandom(numericCharacters));
-//   }
-
-//   if (criteria.includeSymbols) {
-//     possibleCharacters = possibleCharacters.concat(symbolCharacters);
-//     guaranteedCharacters.push(getRandom(symbolCharacters));
-//   }
-
-//   for (var i = 0; i < criteria.length; i++) {
-//     var possibleCharacter = getRandom(possibleCharacters);
-
-//     result.push(possibleCharacter);
-//   }
-
-//   console.log("https://github.com/escowin/password-generator");
-//   return result.join('');
-// }
-
-// Write password to the #password input
-// var writePassword = function() {
-//   // password invokes generatePassword() for its value
-//   var password = generatePassword();
-//   // passwordText selects <#password> from document
-//   var passwordText = document.querySelector("#password");
-
-//   // populates <#password> with password variable value
-//   passwordText.value = password;
-// }
 
 
 // logic.current-date
